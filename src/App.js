510b0e1a546eeaf6f09cedb4711dd826082aeb99
minhnc912 <minhnguyen91200@gmail.com>
2023-04-07 14:33:07 +0700

    Finish simple weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cityName, setCityName] = useState("");
+    const [apiData, setApiData] = useState({});
+
+    const apiKey = "4d5dcffae85f1cb3c76cee23eb096851";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    const fetchData = () => {
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => setApiData(data));
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        fetchData();
+        setCityName("");
+    };
+
+    const cityInputHandler = (e) => {
+        setCityName(e.target.value);
+    };
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                    name="cityInput"
+                    type="text"
+                    onChange={cityInputHandler}
+                    value={cityName}
+                    placeholder="City Name..."
+                ></input>
+                <input type="submit"></input>
+            </form>
+
+            <div>
+                {apiData.main && (
+                    <div className="weather__list">
+                        <div>
+                            <h3>Status: </h3>
+                            <p> {apiData.weather[0].main}</p>
+                        </div>
+
+                        <div>
+                            <h3>Description: </h3>{" "}
+                            <p> {apiData.weather[0].description}</p>
+                        </div>
+                        <div>
+                            <h3>Min Temp: </h3>{" "}
+                            <p> {apiData.main.temp_min}°C</p>
+                        </div>
+                        <div>
+                            <h3>Max Temp: </h3>{" "}
+                            <p> {apiData.main.temp_max}°C</p>
+                        </div>
+                        <img
+                            src={`https://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png`}
+                        ></img>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
